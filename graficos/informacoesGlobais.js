458da0276ac7d3ca986ddf20a4ab3bf5ce1a25e4
Cawani <135828664+Cawani@users.noreply.github.com>
2024-10-29 15:10:11 +0000

    bgfhgf

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,7 +8,7 @@ const dados = await res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     
-    paragrafo.innerHTML = `Em meio a um bilhão de pesssoas conectadas <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    paragrafo.innerHTML = `As pessoas  gastam cerca de 2h e 38 minutos dentre elas estão no Instagram 5,04.10^9 em um total de 7,988.10^9 <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
     console.log(paragrafo)
 
     const container = document.getElementById('graficos-container')
